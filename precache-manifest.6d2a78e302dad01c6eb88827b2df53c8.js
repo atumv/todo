50db4f1bb5b1b8a19a85729c11696152e839492b
atumv <atumv.gm@gmail.com>
2021-01-31 23:29:58 +0700

    Updates

---
+++ precache-manifest.6d2a78e302dad01c6eb88827b2df53c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4331b57fa6724236b1c774b75a47afc5",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "1d978071da6811d06b2a",
+    "url": "/todo/static/css/main.7e697d0e.chunk.css"
+  },
+  {
+    "revision": "2f012bb453fd2dab68ca",
+    "url": "/todo/static/js/2.7bb4b312.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.7bb4b312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d978071da6811d06b2a",
+    "url": "/todo/static/js/main.e03ed1b5.chunk.js"
+  },
+  {
+    "revision": "66217a697004e3fafdc8",
+    "url": "/todo/static/js/runtime-main.fee4e0c2.js"
+  }
+]);
